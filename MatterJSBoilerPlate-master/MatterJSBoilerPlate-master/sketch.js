f8878ae48bc838109863ad0c0fda09a592576c62
ARPotatoHead <72799205+ARPotatoHead@users.noreply.github.com>
2021-05-30 16:56:37 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbinObj,paperObj,groundObj
+
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbinObj=new Dustbin(1000, 650);
+	paperObj=new Paper(200, 450, 40);
+	groundObj=new Ground(600, 675, 1200, 20);
+	var render=Render.create({
+		element:document.body,
+		engine:engine,
+		options:{
+			width:1200,
+			height:700,
+			wireframes:false,
+		}
+	})
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  dustbinObj.display()
+  paperObj.display()
+  groundObj.display()
+  drawSprites();
+ 
+}
+
+
+
